b39efd05473c5546bf292f789174cc73bdbbc54a
Documenter.jl <documenter@juliadocs.github.io>
2021-10-21 21:24:04 +0000

    build based on 0359b08

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = DemoPackagePM","category":"page"},{"location":"#DemoPackagePM","page":"Home","title":"DemoPackagePM","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for DemoPackagePM.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [DemoPackagePM]","category":"page"}]
+}
